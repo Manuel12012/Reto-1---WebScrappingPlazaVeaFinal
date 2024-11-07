ed363556753be4bb6118c4a98b409dfd926750ca
Manuel Meza <manuelmezarivas120@gmail.com>
2024-11-07 17:51:17 -0500

    firts commit

---
+++ background.js
@@ -0,0 +1,17 @@
+chrome.runtime.onConnect.addListener(function(port){
+    port.onMessage.addListener(function(msg){
+        if(msg.cmd==="finish-scrap"){
+            const {products} = msg;
+            chrome.storage.local.set({"products": products}).then(()=>{
+                console.log("Value is set");
+            });
+        }
+        if(msg.cmd === "get-products"){
+            chrome.storage.local.get(["products"]).then((result)=>{
+                port.postMessage({cmd:"result-products",result});
+            });
+        }
+
+    });
+
+});
